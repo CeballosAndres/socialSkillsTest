408a49b77a216704ead3750682b8ae977050862d
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2021-06-04 16:51:13 -0500

    add summary table

--- lib.js
+++ lib.js
@@ -1,3 +1,5 @@
+const totalGroups = [32, 24, 28, 36, 48, 32];
+const groupsLabels = ['Grupo I', 'Grupo II', 'Grupo III', 'Grupo IV', 'Grupo V', 'Grupo VI'];
 const questions = {
     1: "Prestas atención a la persona que te está hablando y haces un esfuerzo para comprender lo que te está diciendo",
     2: "Hablas con los demás de temas poco importantes para pasar luego a los más importantes",
@@ -20,7 +22,7 @@ const questions = {
     19: "Permites que los demás sepan que te interesas o preocupas por ellos",
     20: "Piensas porqué estás asustado y haces algo para disminuir tu miedo",
     21: "Te dices a ti mismo o haces cosas agradables cuando te mereces una recompensa",
-    22: "Reconoces cuando es necesario pedir permiso para hacer algo y luego lo pides a la perso- na indicada",
+    22: "Reconoces cuando es necesario pedir permiso para hacer algo y luego lo pides a la persona indicada",
     23: "Te ofreces para compartir algo que es apreciado por los demás",
     24: "Ayudas a quien lo necesita",
     25: "Llegas a establecer un sistema de negociación que te satisface tanto a ti mismo como a quienes sostienen posturas diferentes",
@@ -39,7 +41,7 @@ const questions = {
     38: "Comprendes la razón por la cual has fracasado en una determinada situación y qué puedes hacer para tener más éxito en el futuro",
     39: "Reconoces y resuelves la confusión que se produce cuando los demás te explican una cosa pero dicen o hacen otras que se contradicen",
     40: "Comprendes lo que significa la acusación y por qué te la han hecho y, luego, piensas en la mejor forma de relacionarte con la persona que te ha hecho la acusación",
-    41: "Planificas la mejor forma para exponer tu punto de vista antes de una conversación pro- blemática",
+    41: "Planificas la mejor forma para exponer tu punto de vista antes de una conversación problemática",
     42: "Decides lo que quieres hacer cuando los demás quieren que hagas otra cosa distinta",
     43: "Resuelves la sensación de aburrimiento iniciando una nueva actividad interesante",
     44: "Reconoces si la causa de algún acontecimiento es consecuencia de alguna situación bajo tu control",
@@ -58,55 +60,62 @@ $(document).ready(function () {
 });
 
 function appendQuestion(key, value) {
-    $('#test').append('' +
-        '<div class="row border-bottom align-items-center">' +
-        '<p class="col-1 text-center">' + key + '</p>' +
-        '<p class="col-7">' + value + '</p>' +
-        '<div class="col-1">' +
-        '<div class="form-check">' +
-        '<input type="radio" class="col-1 form-check-input" name="' + key + '" id="1" required>' +
-        '<label for="test-' + key + '" class="form-check-label"></label>' +
-        '</div>' +
-        '</div>' +
-        '<div class="col-1">' +
-        '<div class="form-check">' +
-        '<input type="radio" class="col-1 form-check-input" name="' + key + '" id="2">' +
-        '<label for="test-' + key + '" class="form-check-label"></label>' +
-        '</div>' +
-        '</div>' +
-        '<div class="col-1">' +
-        '<div class="form-check">' +
-        '<input type="radio" class="col-1 form-check-input" name="' + key + '" id="3">' +
-        '<label for="test-' + key + '" class="form-check-label"></label>' +
-        '</div>' +
-        '</div>' +
-        '<div class="col-1">' +
-        '<div class="form-check">' +
-        '<input type="radio" class="col-1 form-check-input" name="' + key + '" id="4">' +
-        '<label for="test-' + key + '" class="form-check-label"></label>' +
-        '</div>' +
-        '</div>' +
-        '</div>'
-    );
+    let markup = '<tr><th scope="row">' + key + '</th>' +
+        '<td>' + value + '</td>';
+
+    for (let i = 0; i < 4; i++) {
+        markup += '<td><input type="radio" class="form-check-input" name="' + key + '" id="' + (i + 1) + '" ></td>';
+    }
+    markup += '</tr>';
+    $('#tableTest').append(markup);
 };
 
+function tableSummary(values, chartValues) {
+    const rowTitles = ['PDO', 'PDM', 'PDP(%)'];
+    const displayValues = [values, totalGroups, chartValues];
+
+    const tableHead = $('#tableHead').html("<th></th>");
+    for (const title of groupsLabels) {
+        tableHead.append('<th>' + title + '</th>');
+    }
+
+    const tableBody = $('#tableBody').html("");
+    for (let row = 0; row < 3; row++) {
+        let markup = "<tr><th scope='row'>" + rowTitles[row] + "</th>";
+        for (let col = 0; col < values.length; col++) {
+            markup += "<td>" + displayValues[row][col] + "</td>";
+        }
+        markup += '</tr>';
+        tableBody.append(markup);
+    }
+}
+
+
 function chart() {
-    const checked = $('input[type=radio]:checked').length;
-    if (checked == 50) {
+    const checked = $('input[type=radio]:checked');
+    if (checked.length == 50) {
         // clean errors
         $('#errors').html("");
         // get checked values
         const values = getValues();
+        // get fixed values for chart
+        const chartValues = computeChartValues([...values]);
+        // fill table summary
+        tableSummary(values, chartValues);
         // make bar chart
-        makeChart(values);
-        console.log("todos llenos - > graficar");
+        makeChart(chartValues);
     } else {
-        console.log("faltan", 50 - checked);
-        $('#errors').html("<strong>Faltan " + (50 - checked) + " preguntas por contestar.");
+        checked.parent().parent().addClass("table-success");
+        const unchecked = 50 - checked.length; 
+        $('#errors').html('<div class="alert alert-warning" role="alert">Falta ' +
+            unchecked + (unchecked === 1 ? ' pregunta' : ' preguntas') +
+            ' por contestar.</div>');
     }
 };
 
-function getValues(){
+
+
+function getValues() {
     let values = [0, 0, 0, 0, 0, 0];
     const radioChecked = $('input[type=radio]:checked');
     for (const radio of radioChecked) {
@@ -133,17 +142,20 @@ function getValues(){
                 break;
         }
     }
-    values[0] = values[0]/32*100;
-    values[1] = values[1]/24*100;
-    values[2] = values[2]/28*100;
-    values[3] = values[3]/36*100;
-    values[4] = values[4]/48*100;
-    values[5] = values[5]/32*100;
     return values;
 }
 
-function makeChart(values){
-    const labels = ['Grupo I', 'Grupo II', 'Grupo III', 'Grupo IV', 'Grupo V', 'Grupo VI'];
+function computeChartValues(values) {
+    let chartValues = values;
+    for (let i = 0; i < chartValues.length; i++) {
+        chartValues[i] = Math.round(chartValues[i] / totalGroups[i] * 10000) / 100;
+    }
+    return chartValues;
+}
+
+function makeChart(values) {
+
+    const labels = groupsLabels;
     const data = {
         labels: labels,
         datasets: [{
@@ -163,7 +175,7 @@ function makeChart(values){
                 'rgb(255, 205, 86)',
                 'rgb(75, 192, 192)',
                 'rgb(54, 162, 235)',
-                'rgb(153, 102, 255)'                ],
+                'rgb(153, 102, 255)'],
             borderWidth: 1
         }]
     };
@@ -178,9 +190,20 @@ function makeChart(values){
             }
         },
     };
+    $('#chart').html("");
+    $('#chart').append('<canvas id="myChart"></canvas>');
 
     var myChart = new Chart(
         document.getElementById('myChart'),
         config
     );
+}
+
+function testing(number) {
+    const radioInputs = $('input[type=radio]');
+    for (radio of radioInputs) {
+        if (radio.id == number ) {
+            radio.checked = true;
+        }
+    }
 }
